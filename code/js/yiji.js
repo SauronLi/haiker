0aa025d2ffc076919f9799d69013a1d6d11bfde2
bgcode <bglhcode@icloud.com>
2023-02-22 11:14:29 +0800

    版本控制

--- code/js/yiji.js
+++ code/js/yiji.js
@@ -1,14 +1,16 @@
-let d = [];
-if (MY_PAGE == 1) {
-    for (var i in yijimenu) {
-        d.push(yijimenu [i])
+function yiji() {
+    let d = [];
+    if (MY_PAGE == 1) {
+        for (var i in yijimenu) {
+            d.push(yijimenu [i])
+        }
     }
+    d.push({
+        title: "yemian",
+        desc: "miaos",
+        url: "hiker://empty##immersiveTheme##autoCache#",
+        pic_url: "https://w.wallhaven.cc/full/l8/wallhaven-l83o92.jpg",
+        col_type: "movie_3"
+    })
+    setResult(d);
 }
-d.push({
-    title: "yemian",
-    desc: "miaos",
-    url: "hiker://empty##immersiveTheme##autoCache#",
-    pic_url: "https://w.wallhaven.cc/full/l8/wallhaven-l83o92.jpg",
-    col_type: "movie_3"
-})
-setResult(d);
