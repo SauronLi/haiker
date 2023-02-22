f1fbdff494f52bbf9bda09ea988f77d63b893d56
bgcode <bglhcode@icloud.com>
2023-02-22 14:05:45 +0800

    设置界面

--- code/main.js
+++ code/main.js
@@ -136,31 +136,55 @@ function sousuo() {
 }
 
 function shezi() {
-    let bb = [];
-    let shezimenu = [{
-        title: "‘‘’’<b><small>更新</small></b>",
+
+        var d = [];
+    let bturl = "https://lanmeiguojiang.com/tubiao/q/";
+    d.push({
+        title: "<b><small>功能</small></b>",
         url: "hiker://empty",
-        pic_url: version.icon+"src/94.png",
         col_type: "avatar",
-    }, {
-        title: "<b>更新</b>",
-        col_type: "text_icon",
-        url: $("#noLoading#").lazyRule(
-        (GX) => {
-            if (getItem(GX, GX == true ? "on" : "off") == "on") {
-                setItem(GX, "off");
+        img: bturl + "94.png",
+    });
+    let ur = "https://lanmeiguojiang.com/tubiao/messy/";
+    let coll = "text_icon";
+    let 名称 = "影视轮播";
+    let 储存 = "轮播";
+    let des = "开启后，频道顶部以窗口轮播影视剧，关闭显示随机图片。";
+
+    d.push({
+        title: "<b>" + 名称 + "</b>",
+        url: $("#noLoading#").lazyRule((储存) => {
+            if (getItem(储存, 储存 == "轮播" ? "on" : "off") == "on") {
+                setItem(储存, "off");
             } else {
-                setItem(GX, "on");
+                setItem(储存, "on");
             }
+            ;
             refreshPage(false);
             return "hiker://empty";
+        }, 储存),
+        img: getItem(储存, 储存 == "轮播" ? "on" : "off") == "on" ? ur + "55.svg" : ur + "63.svg",
+        col_type: coll,
+        extra: {
+            longClick: [{
+                title: des,
+                js: $.toString((des) => {
+                    return "toast://" + des;
+                }, des)
+            }]
         }
-    )("轮播"),
-        pic_url: version.icon+"src/63.png",
+    });
 
-    }]
-    for (var i in shezimenu) {
-        bb.push(shezimenu [i])
-    }
-    setResult(bb);
+
+    d.push({
+        col_type: "line_blank"
+    })
+
+    d.push({
+        title: "<b><small>播放</small></b>",
+        url: "hiker://empty",
+        col_type: "avatar",
+        img: bturl + "93.png",
+    });
+    setResult(d);
 }
