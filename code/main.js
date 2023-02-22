bc94f19e039294f087e8bafef271602b1b34058a
bgcode <bglhcode@icloud.com>
2023-02-22 16:28:15 +0800

    设置界面

--- code/main.js
+++ code/main.js
@@ -98,7 +98,7 @@ function sousuo() {
 }
 
 function shezi() {
-    // require(version.url + "js/method.js");
-    require(version.url + "/test/learn/she.js");
-    // shezhi();
+    require(version.url + "js/method.js");
+    // require(version.url + "/test/learn/she.js");
+    shezhi();
 }
