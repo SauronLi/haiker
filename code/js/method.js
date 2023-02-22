6bd47908c1c0524b8fbf24bde972f6c56b25c0fe
bgcode <bglhcode@icloud.com>
2023-02-22 14:18:44 +0800

    设置界面

--- code/js/method.js
+++ code/js/method.js
@@ -19,3 +19,55 @@ function Version(){
 function search(search){
     fetch(url);
 }
+
+
+function shezhi(){
+    var d = [];
+    d.push({
+        title: "<b><small>功能</small></b>",
+        url: "hiker://empty",
+        col_type: "avatar",
+        img: version.localhost + "src/94.png",
+    });
+    let ur = version.localhost+"svg/";
+    let name = "更新设置";
+    let arrary = "轮播";
+    let des = "开启后，频道顶部以窗口轮播影视剧，关闭显示随机图片。";
+
+    d.push({
+        title: "<b>" + name + "</b>",
+        url: $("#noLoading#").lazyRule((arrary) => {
+            if (getItem(arrary, arrary == "轮播" ? "on" : "off") == "on") {
+                setItem(arrary, "off");
+            } else {
+                setItem(arrary, "on");
+            }
+            log(getItem(arrary))
+            refreshPage(false);
+            return "hiker://empty";
+        }, arrary),
+        img: getItem(arrary, arrary == "轮播" ? "on" : "off") == "on" ? ur + "55.svg" : ur + "63.svg",
+        col_type: text_icon,
+        extra: {
+            longClick: [{
+                title: des,
+                js: $.toString((des) => {
+                    return "toast://" + des;
+                }, des)
+            }]
+        }
+    });
+
+
+    d.push({
+        col_type: "line_blank"
+    })
+
+    d.push({
+        title: "<b><small>播放</small></b>",
+        url: "hiker://empty",
+        col_type: "avatar",
+        img: bturl + "93.png",
+    });
+    setResult(d);
+}
--- code/main.js
+++ code/main.js
@@ -134,55 +134,6 @@ function sousuo() {
 }
 
 function shezi() {
-
-        var d = [];
-    let bturl = "https://lanmeiguojiang.com/tubiao/q/";
-    d.push({
-        title: "<b><small>功能</small></b>",
-        url: "hiker://empty",
-        col_type: "avatar",
-        img: bturl + "94.png",
-    });
-    let ur = "https://lanmeiguojiang.com/tubiao/messy/";
-    let coll = "text_icon";
-    let 名称 = "影视轮播";
-    let 储存 = "轮播";
-    let des = "开启后，频道顶部以窗口轮播影视剧，关闭显示随机图片。";
-
-    d.push({
-        title: "<b>" + 名称 + "</b>",
-        url: $("#noLoading#").lazyRule((储存) => {
-            if (getItem(储存, 储存 == "轮播" ? "on" : "off") == "on") {
-                setItem(储存, "off");
-            } else {
-                setItem(储存, "on");
-            }
-            ;
-            refreshPage(false);
-            return "hiker://empty";
-        }, 储存),
-        img: getItem(储存, 储存 == "轮播" ? "on" : "off") == "on" ? ur + "55.svg" : ur + "63.svg",
-        col_type: coll,
-        extra: {
-            longClick: [{
-                title: des,
-                js: $.toString((des) => {
-                    return "toast://" + des;
-                }, des)
-            }]
-        }
-    });
-
-
-    d.push({
-        col_type: "line_blank"
-    })
-
-    d.push({
-        title: "<b><small>播放</small></b>",
-        url: "hiker://empty",
-        col_type: "avatar",
-        img: bturl + "93.png",
-    });
-    setResult(d);
+    require(version.localhost + "js/method.js");
+    shezhi();
 }
