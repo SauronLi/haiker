9611b0c191fea0d62b39186144292c0a9587e847
bgcode <bglhcode@icloud.com>
2023-02-23 10:47:42 +0800

    设置界面

---
+++ code/test/down.js
@@ -0,0 +1,24 @@
+var b = ["146", "97", "321", "152", "169", "179", "343", "186"];
+var c = ["system/", "movie/", "erdi/", "music/", "q/", "ke/", "more/", "messy/"];
+var d = [".svg", ".png", ".png", ".svg", ".png", ".png", ".png", ".svg"]
+var url = "https://lanmeiguojiang.com/tubiao/";
+var localhost = "hiker://files/rules/bgHouse/src/"
+
+function yiji() {
+    for (let i in b) {
+        var num = parseInt(b[i]);
+        for (let j = 1; j = num; j++) {
+            MY_URL1 = url + c[i] + j + d[i];
+            MY_URL2 = localhost + c[i] + j + d[i];
+            downloadFile(MY_URL1, MY_URL2);
+        }
+        log(i)
+    }
+    var a
+    a.push({
+        title: 'Download',
+        col_type: 'movie_3',
+
+    })
+    setResult(a);
+}
