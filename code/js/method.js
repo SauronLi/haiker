a796b229c5fe0a4a50f0661d072ee0dda7ccd53b
bgcode <bglhcode@icloud.com>
2023-02-23 18:06:28 +0800

    更新

--- code/js/method.js
+++ code/js/method.js
@@ -105,8 +105,8 @@ function search(d) {
             desc: XQ[i].replace("\r\n", ""),
             pic_url: TP[i],
             url: $("hiker://empty#noRecordHistory##noHistory#").rule(() => {
-                // require("hiker://files/rules/bgHouse/js/erji.js");
-                // main();
+                require("hiker://files/rules/bgHouse/js/erji.js");
+                main();
                 log("zhanshi")
             }),
             col_type: 'movie_2',
