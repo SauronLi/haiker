02dd724642b34185d5398e8b14e4c00703c22b8b
bgcode <bglhcode@icloud.com>
2023-02-24 10:09:07 +0800

    更新

--- code/test/down.js
+++ code/test/down.js
@@ -7,28 +7,74 @@ var localhost = "hiker://files/rules/bgHouse/src/"
 function yiji() {
     download();
     var a = []
-    for (let i = 2; i < b.length; i++) {
-        var num = parseInt(b[i]);
-        for (let j = 1; j < num + 1; j++) {
-            try {
-                pic = localhost + c[i] + j + d[i];
-                a.push({
-                    title: j + d[i],
-                    url: $("#noLoading#").lazyRule((pic) => {
-                        copy(pic);
-                        return "hiker://empty";
-                    }, pic),
-                    pic_url: pic,
-                    col_type: 'icon_small_4',
-                })
-            } catch (e) {
-                log(e)
-            }
-        }
-        log(i)
+    let b = ["系统", "影视", "2D", "音乐", "Q版", "可爱", "更多", "其他"];
+    for (let i in b) {
+        d.push({
+            title: b[i] === getVar('icon', '影视') ? b[i] : b[i],
+            url: $().lazyRule((name, key) => {
+                putVar('icon', name);
+                putVar('iconkey', key);
+                refreshPage(true);
+                return 'hiker://empty'
+            }, b[i], c[i]),
+            col_type: 'scroll_button'
+
+        })
+    }
+    var num = 0;
+    var type = '';
+    var url_icon = '';
+
+    function url(a) {
+        num = parseInt(b[a]);
+        type = c[a];
+        url_icon = d[a]
     }
 
+    switch (getVar('icon', '系统')) {
+        case "系统":
+            url(0);
+            break;
+        case"影视":
+            url(1);
+            break;
+        case"2D":
+            url(2);
+            break;
+        case"音乐":
+            url(3);
+            break;
+        case"Q版":
+            url(4);
+            break;
+        case"可爱":
+            url(5);
+            break;
+        case"更多":
+            url(6);
+            break;
+        case"其他":
+            url(7);
+            break;
+    }
 
+    for (let j = 1; j < num + 1; j++) {
+        try {
+            let pic = localhost + type + j + url_icon;
+            a.push({
+                title: j + d[i],
+                url: $("#noLoading#").lazyRule((pic) => {
+                    copy(pic);
+                    return "hiker://empty";
+                }, pic),
+                pic_url: pic,
+                col_type: 'icon_small_4',
+            })
+        } catch (e) {
+            log(e)
+        }
+
+    }
     setResult(a);
 }
 
@@ -37,13 +83,15 @@ function Version() {
 }//版本更新
 
 function download() {
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < b.length; i++) {
         var num = parseInt(b[i]);
         for (let j = 1; j < num + 1; j++) {
+            let url1 = url + c[i] + j + d[i];
+            let pic = localhost + c[i] + j + d[i];
             try {
-                url1 = url + c[i] + j + d[i];
-                pic = localhost + c[i] + j + d[i];
-                downloadFile(url1, pic);
+                if (!fileExist(pic)) {
+                    downloadFile(url1, pic);
+                }
             } catch (e) {
                 log(e + url1)
             }
