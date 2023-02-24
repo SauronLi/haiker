2a9186e701baf3029e81db4579e086f5f806a9e7
bgcode <bglhcode@icloud.com>
2023-02-24 16:22:09 +0800

    更新

--- code/update.js
+++ code/update.js
@@ -1,26 +1,49 @@
 function password() {
-   // require('')//本地文件设置密码｜直接写死
-    var password='string';//字符串直接写死
-    if(getItem("key","")==""){
-        var d=[];
-
+    // require('')//本地文件设置密码｜直接写死
+    var path = "hiker://files/rules/bgHouse/json/password.json";
+    var password = 'string';//字符串直接写死
+    if (!fileExcist(path)) {
+        var d = [];
         d.push({
             title: '输入密码',
             col_type: 'text_3',
             url: $('密码').input((password) => {
-                if (password==input) {
-                    setItem("key",input);
-                    refreshPage(false);
+                if (password == input) {
+                    try {
+                        let ret = {key: input};
+                        writeFile(filePath, JSON.stringify(ret));
+                        refreshPage(false);
+                    } catch (e) {
+                        'toast://保存出错'
+                    }
                     return 'toast://密码已经保存可使用'
                 } else {
                     return 'toast://密码错误请重试'
                 }
             })
-        },password);
+        }, password);
         setResult(d)
+        return false;
+    } else {
+        let a = JSON.parse(code)
+        if (a.key == input) {
+            log(a.key)
+            return true;
+        }
+        return false;
     }
 }
 
+function yiji() {
+    if (password()) {
+
+        setResult([{
+            title: "sfdaavusbacb\nasdacdiabciabbcidbcbibdscbsabc\nadasdai",
+            col_type: "rich_text"
+        }])
+    }
+
+}
 
 function update() {
     let key = "day"//修改即可重置更新时间
