e192fbfb247cd9a9aa73f4837b0700ac7faabd68
bgcode <bglhcode@icloud.com>
2023-02-25 14:41:07 +0800

    更新

--- code/admin/zhuye.js
+++ code/admin/zhuye.js
@@ -1,31 +1,8 @@
 function main() {
 
     let path = "https://ghproxy.com/https://raw.githubusercontent.com/Codebglh/haiker/main/code/admin/config.js";
-    let picurl = "https://www.dmoe.cc/random.php?t=" + Math.random()
-    let texturl = text()
+
     require(path)
     setResult(a);
 }
 
-function text(){
-    var items = ["https://v1.hitokoto.cn/?encode=text"];
-    var colors = ['grey', '#ff7512', '#cb54ff', '#ff5358', '#ff9044', '#7678ff', '#34b1ff'];
-
-    function getOne(name) {
-        return name[Math.floor(Math.random() * items.length)]
-    }
-
-    let word = "此情若是长久时,网络不好可不行";
-    var tips = '<small><font color="' + getOne(colors) + '">' + word + '</font></small>';
-    try {
-        let word = request(getOne(items), {timeout: 500});
-        if (word.length < 2) {
-           return  tips
-        } else {
-          return   '<small><font color="' + getOne(colors) + '">' + word + '</font></small>'
-        }
-    } catch (e) {
-        return tips
-    }
-
-}
