0e084e1a5bcff893564aee3643bb2e912f4eb1a7
bgcode <bglhcode@icloud.com>
2023-02-25 22:15:41 +0800

    更新

--- code/src/main.js
+++ code/src/main.js
@@ -6,7 +6,7 @@ try {
     if (!fileExist(home)) {
         downloadFile(jpath, home);
     }else{
-        let farversion = "0.0.3";
+        let farversion = "0.0.4";
         let a = JSON.parse(fetch(home));
         var localversion =a[0].version
 
