ea641fbe9e38c872ed1717b16486fad63a250b32
bgcode <bglhcode@icloud.com>
2023-02-25 10:59:23 +0800

    更新

--- code/s.js
+++ code/s.js
@@ -128,23 +128,25 @@ function password() {
     // require('')//本地文件设置密码｜直接写死
     var path = "hiker://files/rules/bgHouse/json/password.json";
     var password = "bgcode666";//字符串直接写死
+    function js(input){
+        eval(getCryptoJS());
+        const j = CryptoJS.enc.Utf8.parse('12cdefglfcdefg12');
+        let j1 = CryptoJS.enc.Utf8.parse(input);
+        let jg = CryptoJS.AES.encrypt(j1, j, {
+            'mode': CryptoJS.mode.ECB, 'padding': CryptoJS.pad.Pkcs7
+        });
+        return  jg.toString()
+    }
     if (!fileExist(path)) {
         var d = [];
         d.push({
             title: '输入密码',
             col_type: 'text_3',
-            url: $('密码').input((psd, filePath) => {
+            url: $('密码').input((psd,filePath) => {
                 if (psd == input) {
-                    require(config.依赖)
-                    eval(getCryptoJS());
-                    const j = CryptoJS.enc.Utf8.parse('12cdefglfcdefg12');
-                    let j1 = CryptoJS.enc.Utf8.parse(input);
-                    let jg = CryptoJS.AES.encrypt(j1, j, {
-                        'mode': CryptoJS.mode.ECB, 'padding': CryptoJS.pad.Pkcs7
-                    });
-                    let url = jg.toString()
-                    let b = {key: url}
-                    var c = [];
+                    let url = js(input)
+                    let b={key: url}
+                    var c =[];
                     c.push(b)
                     writeFile(filePath, JSON.stringify(c));
                     refreshPage(false);
@@ -152,7 +154,7 @@ function password() {
                 } else {
                     return 'toast://密码错误请重试'
                 }
-            }, password, path)
+            }, password,path)
         });
         setResult(d)
         return false;
@@ -160,10 +162,11 @@ function password() {
         let a = JSON.parse(fetch(path))
         if (a[0].key == js(password)) {
             return true;
-        } else {
+        }else {
             deleteFile(path);
             return false;
         }
 
     }
 }
+
